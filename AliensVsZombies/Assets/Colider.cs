bcd95ce45ef657a3ec53610bf40cd348c6ab9fd6
pvnll98 <leopavanello1998@gmail.com>
2015-08-26 18:59:37 -0300

    Game

---
+++ AliensVsZombies/Assets/Colider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    
+    }
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(gameObject.name + " just collided with " + col.gameObject.name);
+        if (col.gameObject.name == "Chao")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
